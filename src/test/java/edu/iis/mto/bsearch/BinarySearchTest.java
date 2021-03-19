962e044a4ebae6da44da8f3d82e4f796c62b1c2f
Stasiak.Jacek <222579@edu.p.lodz.pl>
2021-03-19 17:26:15 +0100

    2. Dodanie testów dla BinarySearch

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class BinarySearchTest {
 
     @BeforeEach
@@ -13,7 +12,50 @@ class BinarySearchTest {
 
     @Test
     void test() {
-        fail("Not yet implemented");
+        int key = -1;
+        try {
+            SearchResult res = BinarySearch.search(key, new int[]{});
+        }catch(IllegalArgumentException e){
+            assertTrue(true);
+        }catch(Exception e){
+            fail("Binary search did not threw correct exception when passed empty array.");
+        }
+
+        fail();
+    }
+
+    @Test
+    void test2() {
+        int key = 50;
+
+        SearchResult res = BinarySearch.search(key, new int[]{ 1, 2, 3, 4, 17, 23});
+
+        assertFalse(res.isFound());
+        assertEquals(-1, res.getPosition());
+    }
+
+
+
+    @Test
+    void test3() {
+        int key = 10;
+        int index = 1;
+
+        SearchResult res = BinarySearch.search(key, new int[]{ 5, 10, 21, 22});
+
+        assertTrue(res.isFound());
+        assertEquals(index, res.getPosition());
+    }
+
+    @Test
+    void test4() {
+        int key = 5;
+        int index = 0;
+
+        SearchResult res = BinarySearch.search(key, new int[]{ 5});
+
+        assertTrue(res.isFound());
+        assertEquals(index, res.getPosition());
     }
 
 }
