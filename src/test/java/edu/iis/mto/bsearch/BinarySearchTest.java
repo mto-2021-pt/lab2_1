a1789f1b08d9e04023417ef1aad933758d690a09
Muras.Wojciech <215828@edu.p.lodz.pl>
2021-03-19 17:25:40 +0100

    testy_v1 Create first test - searching for existing element in one element sequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,19 +1,20 @@
 package edu.iis.mto.bsearch;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 
 class BinarySearchTest {
 
-    @BeforeEach
-    void setUp() throws Exception {}
+    private BinarySearch binarySearch = BinarySearch.create();
+    private static final int[] oneElemSeq = {1};
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    public void searchForExistingElem_singleElemSeq_positionFound() {
+        int searchingElem = 1;
+        int expectedPosition = 0;
+        SearchResult searchResult = binarySearch.search(searchingElem, oneElemSeq);
+        Assertions.assertTrue(searchResult.isFound());
+        Assertions.assertEquals(expectedPosition, searchResult.getPosition());
     }
 
 }
