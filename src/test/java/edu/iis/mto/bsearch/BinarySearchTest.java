2a87fc66be78d1f17f95c99d94ab342b5f4f445e
klesniewski50 <63211654+klesniewski50@users.noreply.github.com>
2021-03-19 19:40:30 +0100

    testy_v2

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -20,6 +20,14 @@ class BinarySearchTest {
     final int[] middleElementSequence = new int[]{3, 4, CORRECT_KEY, 5, 6};
     final int[] noElementSequence = new int[]{3, 4, 5, 6};
 
+    //my inputs
+    final int[] correctWithZerosSequence = new int[]{0, 0, CORRECT_KEY, 0, 0};
+    final int[] onlyZerosSequence = new int[]{0, 0, 0, 0, 0};
+    final int[] correctWithNegativeValuesSequence = new int[]{-1, -2, CORRECT_KEY, -3, -4};
+    final int[] onlyNegativeValuesSequence = new int[]{-1, -2, -3, -4, -5};
+    final int[] multipleCorrectValuesSequence = new int[]{6, CORRECT_KEY, CORRECT_KEY, CORRECT_KEY, 7};
+    final int[] onlyCorrectValuesSequence = new int[]{CORRECT_KEY, CORRECT_KEY, CORRECT_KEY, CORRECT_KEY, CORRECT_KEY};
+
     @BeforeEach
     void setUp() throws Exception {}
 
@@ -62,4 +70,39 @@ class BinarySearchTest {
         assertFalse(result.isFound());
     }
 
+    @Test
+    void correctWithZerosSequenceTest(){
+        SearchResult result = BinarySearch.search(CORRECT_KEY, correctWithZerosSequence);
+        assertTrue(result.isFound());
+    }
+
+    @Test
+    void onlyZerosSequenceTest(){
+        SearchResult result = BinarySearch.search(CORRECT_KEY, onlyZerosSequence);
+        assertFalse(result.isFound());
+    }
+
+    @Test
+    void correctWithNegativeValuesSequenceTest(){
+        SearchResult result = BinarySearch.search(CORRECT_KEY, correctWithNegativeValuesSequence);
+        assertTrue(result.isFound());
+    }
+
+    @Test
+    void onlyNegativeValuesSequenceTest(){
+        SearchResult result = BinarySearch.search(CORRECT_KEY, onlyNegativeValuesSequence);
+        assertFalse(result.isFound());
+    }
+
+    @Test
+    void multipleCorrectValuesSequenceTest() {
+        SearchResult result = BinarySearch.search(CORRECT_KEY, multipleCorrectValuesSequence);
+        assertTrue(result.isFound());
+    }
+
+    @Test
+    void onlyCorrectValuesSequenceTest() {
+        SearchResult result = BinarySearch.search(CORRECT_KEY, onlyCorrectValuesSequence);
+        assertTrue(result.isFound());
+    }
 }
