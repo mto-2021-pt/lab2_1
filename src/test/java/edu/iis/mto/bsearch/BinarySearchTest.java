cf1dc68994240ff4d6b8f32e6f085746b70feec8
Muras.Wojciech <215828@edu.p.lodz.pl>
2021-03-19 18:02:04 +0100

    testy_v2 Refactor test class

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -8,58 +8,56 @@ class BinarySearchTest {
     private BinarySearch binarySearch = BinarySearch.create();
     private static final int[] oneElemSeq = {1};
     private static final int[] multiElemSeq = {1, 2, 3, 4, 5};
+    private static final int firstPosition = 0;
+    private static final int lastPosition = 4;
+    private static final int middlePosition = 2;
+    private static final int notFound = -1;
 
     @Test
     public void searchForExistingElem_singleElemSeq_positionFound() {
         int searchingElem = 1;
-        int expectedPosition = 0;
         SearchResult searchResult = binarySearch.search(searchingElem, oneElemSeq);
         Assertions.assertTrue(searchResult.isFound());
-        Assertions.assertEquals(expectedPosition, searchResult.getPosition());
+        Assertions.assertEquals(firstPosition, searchResult.getPosition());
     }
 
     @Test
     public void searchForNonexistentElem_singleElemSeq_positionNotFound() {
         int searchingElem = 2;
-        int expectedPosition = -1;
         SearchResult searchResult = binarySearch.search(searchingElem, oneElemSeq);
         Assertions.assertFalse(searchResult.isFound());
-        Assertions.assertEquals(expectedPosition, searchResult.getPosition());
+        Assertions.assertEquals(notFound, searchResult.getPosition());
     }
 
     @Test
     public void searchForExistingElem_firstPosition_multiElemSeq_positionFound() {
         int searchingElem = 1;
-        int expectedPosition = 0;
         SearchResult searchResult = binarySearch.search(searchingElem, multiElemSeq);
         Assertions.assertTrue(searchResult.isFound());
-        Assertions.assertEquals(expectedPosition, searchResult.getPosition());
+        Assertions.assertEquals(firstPosition, searchResult.getPosition());
     }
 
     @Test
     public void searchForExistingElem_lastPosition_multiElemSeq_positionFound() {
         int searchingElem = 5;
-        int expectedPosition = 4;
         SearchResult searchResult = binarySearch.search(searchingElem, multiElemSeq);
         Assertions.assertTrue(searchResult.isFound());
-        Assertions.assertEquals(expectedPosition, searchResult.getPosition());
+        Assertions.assertEquals(lastPosition, searchResult.getPosition());
     }
 
     @Test
     public void searchForExistingElem_middlePosition_multiElemSeq_positionFound() {
         int searchingElem = 3;
-        int expectedPosition = 2;
         SearchResult searchResult = binarySearch.search(searchingElem, multiElemSeq);
         Assertions.assertTrue(searchResult.isFound());
-        Assertions.assertEquals(expectedPosition, searchResult.getPosition());
+        Assertions.assertEquals(middlePosition, searchResult.getPosition());
     }
 
     @Test
     public void searchForNonexistentElem_multiElemSeq_positionNotFound() {
         int searchingElem = 6;
-        int expectedPosition = -1;
         SearchResult searchResult = binarySearch.search(searchingElem, multiElemSeq);
         Assertions.assertFalse(searchResult.isFound());
-        Assertions.assertEquals(expectedPosition, searchResult.getPosition());
+        Assertions.assertEquals(notFound, searchResult.getPosition());
     }
 }
