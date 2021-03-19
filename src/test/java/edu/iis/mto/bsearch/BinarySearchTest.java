6b78084f2813a0d20b025ea688d8484ec63b6e06
Muras.Wojciech <215828@edu.p.lodz.pl>
2021-03-19 18:25:52 +0100

    testy_v3 Add test - searching for existing elem in unordered multi elem seq

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -9,6 +9,7 @@ class BinarySearchTest {
     private static final int[] oneElemSeq = {1};
     private static final int[] multiElemSeq = {1, 2, 3, 4, 5};
     private static final int[] duplicatedElemSeq = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+    private static final int[] unorderedElemSeq = {5, 3, 2, 4, 7, 1, 6};
     private static final int[] emptySeq = {};
     private static final int firstPosition = 0;
     private static final int lastPosition = 4;
@@ -81,5 +82,13 @@ class BinarySearchTest {
         SearchResult searchResult = binarySearch.search(searchingElem, duplicatedElemSeq);
         Assertions.assertTrue(searchResult.isFound());
         Assertions.assertEquals(searchingElem, duplicatedElemSeq[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void searchForExistingElem_unorderedMultiElemSeq_positionNotFound() {
+        int searchingElem = 1;
+        SearchResult searchResult = binarySearch.search(searchingElem, unorderedElemSeq);
+        Assertions.assertFalse(searchResult.isFound());
+        Assertions.assertEquals(notFound, searchResult.getPosition());
     }
 }
