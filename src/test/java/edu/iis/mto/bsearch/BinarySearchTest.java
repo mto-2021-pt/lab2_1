89403a22fb5334b662de078df49cafeeaaed9350
221003 <221003@edu.p.lodz.pl>
2021-03-20 00:24:06 +0100

    testy_V1 Dodanie testu dla sekwencji o długości 1 gdzie wartość powinna zostać znaleziona

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -8,9 +8,22 @@ import org.junit.jupiter.api.Test;
 
 class BinarySearchTest {
 
+	private static final int expectedResult = 0;
+	private int[] seq = {1, 2, 3, 4, 5, 6, 7};
+	private int[] oneElementSeq = {2};
+	
+	private SearchResult result;
+	
     @BeforeEach
     void setUp() throws Exception {}
 
+    @Test
+    void searchShouldReturnIndexOfFirstElementInSequenceWithOneElement() {
+    	result = BinarySearch.search(2, oneElementSeq);
+    	assertEquals(result.getPosition(), 0);
+    	assertTrue(result.isFound());
+    }
+    
     @Test
     void test() {
         fail("Not yet implemented");
