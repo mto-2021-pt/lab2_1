cb873a5cf29f86fc104ba6f7769f4b2666fd6ab2
Jakub Wlaźlak <222611@edu.p.lodz.pl>
2021-03-19 21:10:21 +0100

    testy_v1 dodano test sprawdzający sytuację w której klucz nie występuje w sekwencji o długości większej niż 1 oraz zmieniono nazwy metod testujących sekwencjie jednoelementowe

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -9,24 +9,23 @@ import org.junit.jupiter.api.Test;
 class BinarySearchTest {
 
     int[] sequence = new int[]{13, 15, 17, 20, 22, 156, 333, 453, 896};
+    int[] shortSequence = new int[]{13};
 
     @BeforeEach
     void setUp() throws Exception {}
 
     @Test
-    void keyIsInSequence() {
+    void keyIsInSequenceWithOneElement() {
         int key = 13;
-        int[] sequence = new int[]{13};
-        SearchResult sr = BinarySearch.search(key, sequence);
+        SearchResult sr = BinarySearch.search(key, shortSequence);
         assertTrue(sr.isFound());
-        assertEquals(key, sequence[sr.getPosition()]);
+        assertEquals(key, shortSequence[sr.getPosition()]);
     }
 
     @Test
-    void keyIsNotInSequence() {
+    void keyIsNotInSequenceWithOneElement() {
         int key = 87;
-        int[] sequence = new int[]{13};
-        SearchResult sr = BinarySearch.search(key, sequence);
+        SearchResult sr = BinarySearch.search(key, shortSequence);
         assertFalse(sr.isFound());
         assertEquals(-1, sr.getPosition());
     }
@@ -53,5 +52,13 @@ class BinarySearchTest {
         SearchResult sr = BinarySearch.search(key, sequence);
         assertTrue(sr.isFound());
         assertEquals(key, sequence[sr.getPosition()]);
+    }
+
+    @Test
+    void keyIsNotInSequence() {
+        int key = 222;
+        SearchResult sr = BinarySearch.search(key, sequence);
+        assertFalse(sr.isFound());
+        assertEquals(-1, sr.getPosition());
     }
 }
