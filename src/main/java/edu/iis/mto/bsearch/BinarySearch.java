08454cdd87a514fd3bedc0f0b629ec7d604f81f6
Karol Szczechowicz <222585@edu.p.lodz.pl>
2021-03-26 15:02:23 +0100

    Dodanie IllegalArgumentException.

--- src/main/java/edu/iis/mto/bsearch/BinarySearch.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearch.java
@@ -25,6 +25,10 @@ public class BinarySearch {
         int start = 0;
         int end = seq.length - 1;
         int center;
+        
+        if(seq.length==0){
+            throw new IllegalArgumentException("seq.length == 0");
+        }
         SearchResult result = new SearchResult();
 
         while (start <= end) {
